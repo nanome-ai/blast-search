1c9a2d9b2dbf972bc96dd61a7b763af764447a6d
unknown <mul023@ucsd.edu>
2020-02-07 14:30:32 -0800

    list can load the complexes in the workspace

--- nanome_similarity_search/SimilaritySearch.py
+++ nanome_similarity_search/SimilaritySearch.py
@@ -5,10 +5,30 @@ class SimilaritySearch(nanome.PluginInstance):
     def start(self):
         self._menu = SimilaritySearchMenu(self)
         self._menu.build_menu()
+        nanome.util.Logs.debug("similarity search plugin started")
 
     def on_run(self):
         self.menu.enabled = True
         self.update_menu(self.menu)
+        self._menu._request_refresh()
+        nanome.util.Logs.debug("on run called")
+
+    def on_complex_added(self):
+        nanome.util.Logs.debug("Complex added: refreshing")
+        self.request_refresh()
+
+    def on_complex_removed(self):
+        nanome.util.Logs.debug("Complex removed: refreshing")
+        self.request_refresh()
+
+    def request_refresh(self):
+        self.request_complex_list(self.on_complex_list_received)
+        nanome.util.Logs.debug("Complex list requested")
+    
+    def on_complex_list_received(self, complexes):
+        Logs.debug("complex received: ", complexes)
+        self._menu.change_complex_list(complexes)
+    
 
 def main():
     plugin = nanome.Plugin('Similarity Search', 'A Nanome plugin to do similarity search using BLAST', 'other', False)
--- nanome_similarity_search/similaritysearch_menu.py
+++ nanome_similarity_search/similaritysearch_menu.py
@@ -8,7 +8,46 @@ class SimilaritySearchMenu():
     def __init__(self, similaritysearch_plugin):
         self._menu = similaritysearch_plugin.menu
         self._plugin = similaritysearch_plugin
+        self._selected_query = None
+
+    def _request_refresh(self):
+        self._plugin.request_refresh()
+
+    def change_complex_list(self,complex_list):
+        Logs.debug("change complex list called")
+
+        self._query_list = []
+
+        def query_pressed(button):
+            self._selected_query = button.complex
+            Logs.debug("complex ",self._selected_query, " selected")
+
+        for complex in complex_list:
+            clone = self._complex_item_prefab.clone()
+            ln_btn = clone.get_children()[0]
+            btn = ln_btn.get_content()
+            btn.text.value.set_all(complex.name)
+            btn.complex = complex
+            btn.register_pressed_callback(query_pressed)
+            self._query_list.append(clone)
+        
+        Logs.debug("query list is:", self._query_list)
+        self._show_list.items = self._query_list
+        self._plugin.update_content(self._show_list)
 
     def build_menu(self):
+        
         menu = nanome.ui.Menu.io.from_json(os.path.join(os.path.dirname(__file__), 'SimilaritySearchMenu.json'))
         self._plugin.menu = menu
+
+        self._show_list = menu.root.find_node("Input List", True).get_content()
+
+        # Create a prefab that will be used to populate the lists
+        self._complex_item_prefab = nanome.ui.LayoutNode()
+        self._complex_item_prefab.layout_orientation = nanome.ui.LayoutNode.LayoutTypes.horizontal
+        child = self._complex_item_prefab.create_child_node()
+        child.name = "complex_button"
+        prefabButton = child.add_new_button()
+        prefabButton.text.active = True
+
+        
