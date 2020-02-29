52a0ca92242e4b51ed7e197cbe56ca81e7c88c41
Limuzhou <mul023@ucsd.edu>
2020-02-28 16:08:06 -0800

    pdb to fasta, error messages, selected label

--- nanome_similarity_search/SimilaritySearch.py
+++ nanome_similarity_search/SimilaritySearch.py
@@ -2,16 +2,23 @@ import nanome
 import os
 from nanome.util import Logs
 from .similaritysearch_menu import SimilaritySearchMenu
+from .similaritysearch_menu import ErrorOptions
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
  
 class SimilaritySearch(nanome.PluginInstance):
+
     def start(self):
         self._menu = SimilaritySearchMenu(self)
         self._menu.build_menu()
         nanome.util.Logs.debug("similarity search plugin started")
         self.number_of_results = 10
         self.result_list = []
+        self.selected_query = None
+        self.letters = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D','CYS':'C','GLU':'E','GLN':'Q','GLY':'G','HIS':'H',
+           'ILE':'I','LEU':'L','LYS':'K','MET':'M','PHE':'F','PRO':'P','SER':'S','THR':'T','TRP':'W',
+           'TYR':'Y','VAL':'V'}
+
 
     def on_run(self):
         self.menu.enabled = True
@@ -35,36 +42,65 @@ class SimilaritySearch(nanome.PluginInstance):
         Logs.debug("complex received: ", complexes)
         self._menu.change_complex_list(complexes)
 
+    def update_selected_query(self,complex):
+        if complex!= None:
+            self.request_complexes([complex.index],self.on_query_received)
+        else:
+            self.selected_query = None
+    
+    def on_query_received(self,complex):
+        self.selected_query = complex[0]
+
+    def pdb_to_fasta(self,complex):
+        fasta_res = ""
+        complex_name = complex.full_name
+        print("chains are: ",complex.chains)
+        print(len(list(complex.chains)))
+        for chain in complex.chains:
+            if not any(atom.is_het for atom in chain.atoms):
+                fasta_res+=(">"+complex_name+":"+chain.name+"\n")
+                for residue in [x.type for x in chain.residues ]:
+                    if residue in self.letters:
+                        fasta_res+=(self.letters[residue])
+                fasta_res+=("\n")
+        return fasta_res
+
+    def update_error_message(self,option):
+        self._menu.update_error_message(option)
+
     def search_blast(self):
         Logs.debug("blast search test started")
-        with open(os.path.join(os.path.dirname(__file__), "1tyl.fasta"),"r") as file:
-            fasta_string = file.read()
-        Logs.debug("the fasta string is: ",fasta_string)
-        result_handle = NCBIWWW.qblast("blastp", "nt", fasta_string)
-        # result_handle = NCBIWWW.qblast("blastn", "nt", "8332116")
-        # with open(os.path.join(os.path.dirname(__file__),"my_blast.xml"), "w") as out_handle:
-        #     out_handle.write(result_handle.read())
-        # result_handle.close()
-        # Logs.debug("blast XML file written")
-        Logs.debug("qblast ended")
-        for x in range(self.number_of_results):
-            if x == 0:
-                try:
-                    blast_record = NCBIXML.read(result_handle)
-                    self.result_list.append(blast_record)
-                    Logs.debug("blast result #1: ",blast_record)
-                except:
-                    Logs.debug("No result found")
-                    break
-            else:
-                try:
-                    blast_record = next(blast_records)
-                    self.result_list.append(blast_record)
-                    Logs.debug("blast result #",x+1," ",blast_record)
-                except:
-                    Logs.debug("No more results")
-                    break
-                
+        # with open(os.path.join(os.path.dirname(__file__), "1tyl.fasta"),"r") as file:
+        #     fasta_string = file.read()
+        # Logs.debug("the fasta string is: ",fasta_string)
+        if self.selected_query:
+            fasta_string = self.pdb_to_fasta(self.selected_query)
+            print(fasta_string)
+            result_handle = NCBIWWW.qblast("blastp", "nt", fasta_string)
+            print("result handle: ",result_handle)
+            Logs.debug("qblast ended")
+            for x in range(self.number_of_results):
+                Logs.debug("iteration ",x+1,": ")
+                if x == 0:
+                    try:
+                        blast_record = NCBIXML.parse(result_handle)
+                        Logs.debug("the #",x+1," result is: ",blast_record)
+                        self.result_list.append(blast_record)
+                    except:
+                        Logs.debug("No result found")
+                        break
+                        Logs.debug("blast result #1: ",blast_record)
+                else:
+                    try:
+                        blast_record = next(blast_records)
+                        self.result_list.append(blast_record)
+                        Logs.debug("blast result #",x+1," ",blast_record)
+                    except:
+                        Logs.debug("No more results")
+                        break
+        else:
+            self.update_error_message(ErrorOptions.unselected)
+
         self.update_result()
 
     def update_result(self):
--- nanome_similarity_search/similaritysearch_menu.py
+++ nanome_similarity_search/similaritysearch_menu.py
@@ -1,14 +1,21 @@
 import nanome
 from nanome.util import Logs
 from nanome.util import Color
-
+from enum import IntEnum
 import os
+class ErrorOptions(IntEnum):
+        clear = 0
+        unselected = 1
 
 class SimilaritySearchMenu():
+    
+
     def __init__(self, similaritysearch_plugin):
         self.menu = similaritysearch_plugin.menu
         self.plugin = similaritysearch_plugin
         self.selected_query = None
+        self.error_option = ErrorOptions.clear
+
 
     def _request_refresh(self):
         self.plugin.request_refresh()
@@ -23,8 +30,13 @@ class SimilaritySearchMenu():
             button.selected = not button.selected
             if button.selected:
                 self.selected_query = button.complex
+                self.update_selected_label()
+                self.update_error_message(ErrorOptions.clear)
+                self.plugin.update_selected_query(button.complex)
             else:
                 self.selected_query = None
+                self.update_selected_label()
+                self.plugin.update_selected_query(None)
             self.plugin.update_content(button)
 
         for complex in complex_list:
@@ -47,13 +59,30 @@ class SimilaritySearchMenu():
             clone = self.complex_item_prefab.clone()
             ln_btn = clone.get_children()[0]
             btn = ln_btn.get_content()
-            btn.text.value.set_all(result.query)
+            print(list(result))
+            btn.text.value.set_all(result.name)
             btn.complex = result
             self.result_list.append(clone)
         Logs.debug("result list is: ",self.result_list)
         self.output_list.items = self.result_list
         self.plugin.update_content(self.output_list)
         pass
+
+    def update_error_message(self,option):
+        #self.error_message.text_auto_size=False
+        # self.error_message.text_size = 0.198
+        if option == ErrorOptions.clear:
+            self.error_message.text_value = ""
+        elif option == ErrorOptions.unselected:
+            self.error_message.text_value = "Error: No query selected"
+        self.plugin.update_content(self.error_message)
+    
+    def update_selected_label(self):
+        if self.selected_query:
+            self.selected_label.text_value = "Selected input: "+ self.selected_query.full_name
+        else:
+            self.selected_label.text_value = "Selected input: "
+        self.plugin.update_content(self.selected_label)
 
     def build_menu(self):
         def search_button_pressed_callback(button):
@@ -77,6 +106,8 @@ class SimilaritySearchMenu():
         self.output_list = menu.root.find_node("Output List",True).get_content()
         self.number_input = menu.root.find_node("Number Input",True).get_content()
         self.number_input.register_changed_callback(number_entered_callback)
+        self.error_message = menu.root.find_node("Error Message", True).get_content()
+        self.selected_label = menu.root.find_node("Label Text",True).get_content()
 
         # Create a prefab that will be used to populate the lists
         self.complex_item_prefab = nanome.ui.LayoutNode()
